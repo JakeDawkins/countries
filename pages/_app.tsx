6116c95d98f2f87dcbe26bc042c54663dc596f54
Jackson Dawkins <dawkinsjh@gmail.com>
2023-01-23 16:46:53 -0500

    setup jest and apollo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,20 @@
 import '../globals.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://countries.trevorblades.com/',
+  cache: new InMemoryCache(),
+});
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />;
+    </ApolloProvider>
+  );
 }
