f732b7833dfeeb2fa475005b1f15d0945bd4564c
Jackson Dawkins <dawkinsjh@gmail.com>
2023-01-23 17:01:55 -0500

    basic request for country

--- pages/country/[code].tsx
+++ pages/country/[code].tsx
@@ -1,15 +1,38 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import { Layout } from '../../components';
+import { gql, useQuery } from '@apollo/client';
+
+const COUNTRY_QUERY = gql`
+  query CountryQuery($code: ID!) {
+    country(code: $code) {
+      name
+    }
+  }
+`;
 
 function CountryDetail() {
   const router = useRouter();
+  const code = router?.query?.code;
+
+  const { loading, data, error } = useQuery(COUNTRY_QUERY, {
+    variables: {
+      code: code?.toUpperCase(),
+    },
+    skip: !code,
+  });
+
+  if (loading || !data) return <p>Loading...</p>;
+  if (error) return <p>Error</p>;
+
   return (
     <Layout>
-      <h1 className="underline text-3xl font-bold">Country Name</h1>
+      <h1 className="underline text-3xl font-bold">
+        {data?.country?.name || 'Not Found'}
+      </h1>
 
       {/* TODO - failure */}
-      <p>Country Code: {router.query.code}</p>
+      <p>Country Code: {code}</p>
     </Layout>
   );
 }
