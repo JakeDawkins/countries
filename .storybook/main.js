efa34b5f72534e982a76a16c491835a688817098
Jackson Dawkins <dawkinsjh@gmail.com>
2023-01-23 18:22:42 -0500

    Add tailwind to storybook build

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,6 +8,19 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    // added based on an example from this SO thread
+    // https://stackoverflow.com/questions/65495912/storybook-tailwind-how-should-i-add-tailwind-to-storybook
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        cssLoaderOptions: {
+          importLoaders: 1,
+        },
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
   framework: '@storybook/react',
   core: {
