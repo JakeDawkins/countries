aa16fb824276d503718d6af839830c41200569e7
Jackson Dawkins <dawkinsjh@gmail.com>
2023-01-24 17:18:21 -0500

    added basic 404 page

---
+++ src/pages/404.page.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import React from 'react';
+import { CountryInfo } from '../components';
+
+function Page404() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold">Page Not Found</h1>
+      <p className="mt-8">
+        This page either doesn't exist or is unavailable right now.
+      </p>
+      <p className="mt-4">
+        <Link href="/" className="underline font-bold">
+          Back to Home
+        </Link>
+      </p>
+    </>
+  );
+}
+
+export default Page404;
